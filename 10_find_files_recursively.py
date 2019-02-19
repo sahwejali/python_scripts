fbb0ec417ddc12d171e2af43e95e0f0c5bb612bd
sahwej <41147038+sahwejali@users.noreply.github.com>
2019-02-19 15:48:21 +0530

    Update 10_find_files_recursively.py

--- 10_find_files_recursively.py
+++ 10_find_files_recursively.py
@@ -25,7 +25,14 @@ def get_file_names(filepath, pattern):
 def output_files(list_of_files):
     for filename in list_of_files:
         print(filename)
-
-
-if __name__ == '__main__':
-    get_file_names(PATH, PATTERN)
+if os.path.exists(filepath);
+for user,dirnames,filenames in os.walk(filepath)
+for filename in fnmatch.filter(fiulenames, pattern):
+    matches.append(os.path.join(filename))
+    if matches:
+        printf("Found {files:.format(len(matches)))
+               output_files(matches)
+               else:
+               print("No files found.")
+               else:
+               print("the path does not exist. try again.")
